f2a6a8885657deeec03d4568ed28156c0fae9b54
sujalraikar2004 <sujalraikar84@gmail.com>
2025-10-26 13:06:46 +0530

    jai shree ram

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig(({ mode }) => ({
     port: 8080,
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://signaturedrapes.in',
         changeOrigin: true,
         secure: false,
       },
