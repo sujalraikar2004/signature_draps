7f3ebd9df3a78597676bfb759999bf6b85c80fa2
sujalraikar2004 <sujalraikar84@gmail.com>
2025-11-01 14:19:54 +0530

    sdbsj

--- src/pages/ContactUs.tsx
+++ src/pages/ContactUs.tsx
@@ -205,7 +205,7 @@ export default function ContactUs() {
               <h3 className="text-xl font-heading font-semibold mb-4">Find Us on Map</h3>
               <div className="aspect-video w-full rounded-lg overflow-hidden">
                 <iframe
-                  src="https://maps.app.goo.gl/3L1WAA2iywzbMX3C9"
+                  src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3847.8!2d74.5!3d15.85!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTXCsDUxJzAwLjAiTiA3NMKwMzAnMDAuMCJF!5e0!3m2!1sen!2sin!4v1635000000000!5m2!1sen!2sin"
                   width="100%"
                   height="100%"
                   style={{ border: 0 }}
@@ -219,7 +219,7 @@ export default function ContactUs() {
                 <Button 
                   variant="outline" 
                   className="w-full"
-                  onClick={() => window.open('https://maps.app.goo.gl/3L1WAA2iywzbMX3C9?g_st=aw', '_blank')}
+                  onClick={() => window.open('https://maps.app.goo.gl/ysSzo27SnUpvks2AA?g_st=aw', '_blank')}
                 >
                   <MapPin className="mr-2 h-4 w-4" />
                   Open in Google Maps
