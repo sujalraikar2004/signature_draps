9752ff50e0295fb983bdf7ca6f046b0fcc53c1dd
Vivekveer31 <veervivek47@gmail.com>
2026-01-14 19:44:33 +0530

    fix(home): ui

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -154,7 +154,7 @@ export function Navbar() {
                 {isAuthenticated && wishlistCount > 0 && (
                   <Badge
                     variant="destructive"
-                    className="h-3 w-3 absolute -top-2 -right-2 md:h-5 md:w-5 rounded-full p-0 flex items-center justify-center text-xs"
+                    className=" absolute -top-2 -right-2 h-5 w-5 rounded-full p-0 flex items-center justify-center text-xs"
                   >
                     {wishlistCount}
                   </Badge>
